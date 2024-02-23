96b1b445cc2eb31ef733b00ff6fd2694070fdd8a
SavvasVinayakKajagar1 <vinayak.kajagar1@savvas.com>
2024-02-24 00:11:14 +0530

    assignment 10 done

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -1,4 +1,5 @@
 import { client } from "..";
+import { QueryResult } from "pg";
 /*
  * Function should insert a new todo for this user
  * Should return a todo object
@@ -9,9 +10,19 @@ import { client } from "..";
  *  id: number
  * }
  */
-export async function createTodo(userId: number, title: string, description: string) {
-    
+export async function createTodo(
+  userId: number,
+  title: string,
+  description: string
+) {
+  const result: QueryResult<any> = await client.query(
+    "INSERT INTO todos (user_id, title, description) VALUES ($1, $2, $3) RETURNING *",
+    [userId, title, description]
+  );
+
+  return result.rows[0];
 }
+
 /*
  * mark done as true for this specific todo.
  * Should return a todo object
@@ -23,7 +34,8 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    const result: QueryResult<any> = await client.query('UPDATE todos SET done = true WHERE id = $1 RETURNING *', [todoId]);
+    return result.rows[0];
 }
 
 /*
@@ -37,5 +49,6 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+    const result: QueryResult<any> = await client.query('SELECT * FROM todos WHERE user_id = $1', [userId]);
+    return result.rows;
 }
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -10,7 +10,8 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    const result = await client.query('INSERT INTO users (username, password, name) VALUES ($1, $2, $3)', [username, password, name]);
+    return result;
 }
 
 /*
@@ -22,5 +23,6 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    const result = await client.query('SELECT * FROM users WHERE id = $1', [userId]);
+    return result.rows[0];
 }
